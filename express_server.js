0ef226c9618666a4fe10000a321bbab8ef745916
nawalabdi <nawalabdi03@gmail.com>
2022-11-29 19:36:27 -0500

    added log out and log in functionality

--- express_server.js
+++ express_server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
+
+
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+
 function generateRandomString() {
   return (Math.random().toString(36).substr(2, 6))
 }
@@ -11,7 +18,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,12 +33,17 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+     urls: urlDatabase, 
+     username: req.cookies.username};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars =
+   { username : req.cookies.username
+  }
+  res.render("urls_new",templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -65,6 +77,20 @@ app.post("/urls/:id/", (req, res) => {
   urlDatabase[req.params.id] = req.body.updatedUrl;
   res.redirect("/urls")
 })
+
+app.post("/login", (req, res) => {
+ console.log(req.body);
+ const userName = req.body.username
+ res.cookie("username", userName)
+ console.log(userName)
+  res.redirect("/urls")
+})
+
+app.post("/logout", (req, res) => {
+  console.log(req.body);
+  res.clearCookie("username")
+   res.redirect("/urls")
+ })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
